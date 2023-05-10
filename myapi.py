2d45fad292a1e2adb3d781695653adbbdaf575d5
qsmsoft <qsmsoft@gmail.com>
2023-05-10 11:02:11 +0500

    added post method

--- myapi.py
+++ myapi.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Path
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -7,9 +8,15 @@ students = {
     1: {
         "name": "john",
         "age": 17,
-        "class": "year 12"
+        "year": "year 12"
     }
 }
+
+
+class Student(BaseModel):
+    name: str
+    age: int
+    year: str
 
 
 @app.get("/")
@@ -28,3 +35,12 @@ def get_student(*, student_id: int, name: Optional[str] = None, test: int):
         if students[student_id]["name"] == name:
             return students[student_id]
     return {"Data": "Not found"}
+
+
+@app.post("/create-student/{student_id}")
+def create_student(student_id: int, student: Student):
+    if student_id in students:
+        return {"Error": "Student exists"}
+
+    students[student_id] = student
+    return students[student_id]
